ebed20a6d748023d830494397597953ce87ce765
Ahmed Magdy <ahmed.magdy40@gmail.com>
2017-10-25 10:55:36 +0200

    [add] django: add serach view
     Search view calles the internal services api to call search from scrapper and cleanse from cleansing services

--- api/api_v1/urls.py
+++ api/api_v1/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import ping_view
+from .views import ping_view, search_view
 
 
 urlpatterns = [
-    url(r'^ping/$', ping_view)
+    url(r'^ping/$', ping_view),
+    url(r'^search/$', search_view)
 ]
--- api/api_v1/views.py
+++ api/api_v1/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-from django.http.response import JsonResponse
+from django.http import JsonResponse
+import os
+import requests
 
 
 def ping_view(req, *args, **kwargs):
     return JsonResponse({'msg': 'pong'})
+
+
+def search_view(req, *args, **kwargs):
+    api_gateway = '{}://{}/'.format(os.environ.get('SCHEME'), os.environ.get('SERVICE_API'))
+    q = req.GET.get('q', 'misc')
+    page = req.GET.get('page', 1)
+    res = requests.get(api_gateway + 'search', params={'q': q, 'page': page})
+    if res.staus_code == 200:
+        return JsonResponse(res.json())
+    else:
+        return JsonResponse({})
